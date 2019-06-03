cc555d415c00b1b5d20712859216d00715e065a6
Corey Moore <monorthwest@gmail.com>
2019-06-03 15:46:40 -0700

    added jedi name function and beyond function

--- Adrienne-Corey-Function-Drills-2/script.js
+++ Adrienne-Corey-Function-Drills-2/script.js
@@ -1,7 +1,32 @@
+'use strict';
+
 function jediName(firstName, lastName) {
-    const name = firstName.slice(0,2);
-    const surname = lastName.slice(0,1);
-     name + surname;
+  const name = firstName.slice(0,2);
+  const surname = lastName.slice(0,3);
+  return surname + name;
+  //console.log(name,surname);
 }
 let output = jediName('Beyonce', 'Knowles');
 console.log(output);
+
+/////////////////////////////////////
+
+function beyond(num){
+  if(num === Infinity || num === -Infinity){
+    console.log('And beyond');
+  }
+  else if(num > 0){
+    console.log('To infinity');
+  }
+  else if(num < 0){
+    console.log('To negative infinity');
+  }
+  else if(num === 0){
+    console.log('Staying home');
+  }
+}
+
+beyond(34);
+
+///////////////////////////////////////
+
